9a2846dc7cf11b9ec42268c3db268ed11d7b4c02
Marco Ciccone <marcociccone88@gmail.com>
2017-05-07 12:16:13 +0200

    Change summary frequency default to 10 steps

--- main_loop_tf/config/flow.py
+++ main_loop_tf/config/flow.py
@@ -28,7 +28,7 @@ gflags.DEFINE_bool('do_validation_only', False, 'If True does one round '
                    'of validation')
 gflags.DEFINE_bool('summary_sublayer', False, 'If True groups summaries '
                    'with two layers deep')
-gflags.DEFINE_integer('train_summary_freq', 1,
+gflags.DEFINE_integer('train_summary_freq', 10,
                       'How frequent save train summaries (in steps)')
-gflags.DEFINE_integer('val_summary_freq', 1,
+gflags.DEFINE_integer('val_summary_freq', 10,
                       'How frequent save validation summaries (in steps)')
