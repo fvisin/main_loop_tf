49e12b721b94e3ff6cf6b6a4ae829a806e8da443
Francesco Visin <fvisin@gmail.com>
2017-11-24 17:34:53 +0100

    Partial update of run_example

--- main_loop_tf/run_example.py
+++ main_loop_tf/run_example.py
@@ -1,25 +1,36 @@
-def build_model(inputs, is_training):
-    from tensorflow.contrib import slim
-    import gflags
+from main_loop_tf import BaseExperiment
 
-    cfg = gflags.cfg
-    conv = slim.conv2d(inputs,
-                       num_outputs=cfg.nclasses,
-                       kernel_size=(1, 1),
-                       stride=1)
 
-    if is_training:
-        return conv
-    else:
-        return conv * 2
+class ThisExperiment(BaseExperiment):
+
+    def build_loss(self, ):
+        return 1
+
+    def build_model(self, inputs, is_training):
+        from tensorflow.contrib import slim
+        import gflags
+
+        cfg = gflags.cfg
+        conv = slim.conv2d(inputs,
+                           num_outputs=cfg.nclasses,
+                           kernel_size=(1, 1),
+                           stride=1)
+
+        if is_training:
+            return conv
+        else:
+            return conv * 2
 
 
 if __name__ == '__main__':
     import sys
-    from main_loop_tf.main import run
 
     # You can also add fixed values like this
     argv = sys.argv
     argv += ['--dataset', 'camvid']
 
-    run(argv, build_model)
+    exp = ThisExperiment(argv)
+    if exp.cfg.do_validation_only:
+        exp.validate()
+    else:
+        exp.run()
