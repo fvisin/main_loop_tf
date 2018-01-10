8b2780a9a5e32eb5a76bd55e16d52e4caefd7d8a
Francesco Visin <fvisin@gmail.com>
2018-01-10 17:36:59 +0100

    Decrease default patience

--- main_loop_tf/config/flow.py
+++ main_loop_tf/config/flow.py
@@ -12,7 +12,7 @@ gflags.DEFINE_integer('min_epochs', 1, 'The minimum number of epochs '
                       'before early stopping is possible', lower_bound=1)
 gflags.DEFINE_integer('max_epochs', 100, 'The maximum number of epochs',
                       lower_bound=1)
-gflags.DEFINE_integer('patience', 100, 'The number of validation with no '
+gflags.DEFINE_integer('patience', 10, 'The number of validation with no '
                       'improvement the model will wait before early stopping',
                       lower_bound=1)
 gflags.DEFINE_bool('do_validation_only', False, 'If True does one round '
